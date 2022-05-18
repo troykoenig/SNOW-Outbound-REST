2a68eabdc4844277d8d9e61fb46474567f3b34c3
troykoenig <troy.r.koenig@gmail.com>
2022-05-18 11:36:34 -0500

    gr.next
    gr.next

--- Scheduled Jobs/GetUsers.js
+++ Scheduled Jobs/GetUsers.js
@@ -4,7 +4,7 @@ var gr = new GlideRecord('sys_user_grmember.do');
 gr.addEncodedQuery('group=679434f053231300e321ddeeff7b12d8');
 gr.query();
 
-while(next()) {
+while(gr.next()) {
     var user = {
         "full_name": gr.getValue('full_name'),
         "email": gr.getValue('email')
